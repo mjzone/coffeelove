2620e8059889c764fc226a826128078895661234
Manoj Fernando <manojf@99x.lk>
2020-08-25 06:00:08 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://media.giphy.com/media/ceeFbVxiZzMBi/giphy.gif" alt="logo" />
+        <h2>I love Coffee!</h2>
       </header>
     </div>
   );
